6c6b60cdcfcc485fdbb5f36426d917d687b6e846
pacifikus <masterkristall@gmail.com>
2021-08-26 01:51:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,15 @@ import sys
 import matplotlib.pyplot as plt 
 import data
 import scipy.io
+import pyLDAvis
+
+from navec import Navec
 
 from torch import nn, optim
 from torch.nn import functional as F
-from pathlib import Path
-from gensim.models.fasttext import FastText as FT_gensim
-import tracemalloc
-
 
 from etm import ETM
-from utils import nearest_neighbors, get_topic_coherence, get_topic_diversity
+from utils import nearest_neighbors, get_topic_coherence, get_topic_diversity, get_coherence_gensim
 
 parser = argparse.ArgumentParser(description='The Embedded Topic Model')
 
@@ -76,28 +75,53 @@ if torch.cuda.is_available():
 
 ## get data
 # 1. vocabulary
-vocab, training_set, valid, test_1, test_2 = data.get_data(doc_terms_file_name="tf_idf_doc_terms_matrix_time_window_1",
-                                                           terms_filename="tf_idf_terms_time_window_1")
+vocab, train, valid, test = data.get_data(os.path.join(args.data_path))
 vocab_size = len(vocab)
 args.vocab_size = vocab_size
 
 # 1. training data
-args.num_docs_train = training_set.shape[0]
+train_tokens = train['tokens']
+train_counts = train['counts']
+args.num_docs_train = len(train_tokens)
 
 # 2. dev set
-
-args.num_docs_valid = valid.shape[0]
+valid_tokens = valid['tokens']
+valid_counts = valid['counts']
+args.num_docs_valid = len(valid_tokens)
 
 # 3. test data
-
-args.num_docs_test = test_1.shape[0] + test_2.shape[0]
-
-args.num_docs_test_1 = test_1.shape[0]
-args.num_docs_test_2 = test_2.shape[0]
+test_tokens = test['tokens']
+test_counts = test['counts']
+args.num_docs_test = len(test_tokens)
+test_1_tokens = test['tokens_1']
+test_1_counts = test['counts_1']
+args.num_docs_test_1 = len(test_1_tokens)
+test_2_tokens = test['tokens_2']
+test_2_counts = test['counts_2']
+args.num_docs_test_2 = len(test_2_tokens)
 
 embeddings = None
 if not args.train_embeddings:
-    embeddings = data.read_embedding_matrix(vocab, device, load_trainned=False)
+    emb_path = args.emb_path
+    navec = Navec.load(emb_path)
+    #vect_path = os.path.join(args.data_path.split('/')[0], 'embeddings.pkl')   
+    #vectors = {}
+    #with open(emb_path, 'rb') as f:
+    #    for l in f:
+    #        line = l.decode().split()
+    #        word = line[0]
+    #        if word in vocab:
+    #            vect = np.array(line[1:]).astype(np.float)
+    #            vectors[word] = vect
+    embeddings = np.zeros((vocab_size, args.emb_size))
+    words_found = 0
+    for i, word in enumerate(vocab):
+        try: 
+            embeddings[i] = navec[word]
+            words_found += 1
+        except KeyError:
+            embeddings[i] = np.random.normal(scale=0.6, size=(args.emb_size, ))
+    embeddings = torch.from_numpy(embeddings).to(device)
     args.embeddings_dim = embeddings.size()
 
 print('=*'*100)
@@ -108,45 +132,220 @@ print('=*'*100)
 if not os.path.exists(args.save_path):
     os.makedirs(args.save_path)
 
-if args.mode == 'eval':
+if args.mode in ['eval', 'ppx']:
     ckpt = args.load_from
 else:
-    ckpt = Path.cwd().joinpath(args.save_path, 
+    ckpt = os.path.join(args.save_path, 
         'etm_{}_K_{}_Htheta_{}_Optim_{}_Clip_{}_ThetaAct_{}_Lr_{}_Bsz_{}_RhoSize_{}_trainEmbeddings_{}'.format(
         args.dataset, args.num_topics, args.t_hidden_size, args.optimizer, args.clip, args.theta_act, 
             args.lr, args.batch_size, args.rho_size, args.train_embeddings))
 
 ## define model and optimizer
-model = ETM(args.num_topics, 
-            vocab_size, 
-            args.t_hidden_size, 
-            args.rho_size, 
-            args.emb_size, 
-            args.theta_act, 
-            embeddings, 
-            args.train_embeddings, 
-            args.enc_drop).to(device)
+model = ETM(args.num_topics, vocab_size, args.t_hidden_size, args.rho_size, args.emb_size, 
+                args.theta_act, embeddings, args.train_embeddings, args.enc_drop).to(device)
 
 print('model: {}'.format(model))
 
-optimizer = model.get_optimizer(args)
+if args.optimizer == 'adam':
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.wdecay)
+elif args.optimizer == 'adagrad':
+    optimizer = optim.Adagrad(model.parameters(), lr=args.lr, weight_decay=args.wdecay)
+elif args.optimizer == 'adadelta':
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr, weight_decay=args.wdecay)
+elif args.optimizer == 'rmsprop':
+    optimizer = optim.RMSprop(model.parameters(), lr=args.lr, weight_decay=args.wdecay)
+elif args.optimizer == 'asgd':
+    optimizer = optim.ASGD(model.parameters(), lr=args.lr, t0=0, lambd=0., weight_decay=args.wdecay)
+else:
+    print('Defaulting to vanilla SGD')
+    optimizer = optim.SGD(model.parameters(), lr=args.lr)
 
+train_losses = []
+val_losses = []
 
-tracemalloc.start()
+def train(epoch):
+    model.train()
+    acc_loss = 0
+    acc_kl_theta_loss = 0
+    cnt = 0
+    indices = torch.randperm(args.num_docs_train)
+    indices = torch.split(indices, args.batch_size)
+    for idx, ind in enumerate(indices):
+        optimizer.zero_grad()
+        model.zero_grad()
+        data_batch = data.get_batch(train_tokens, train_counts, ind, args.vocab_size, device)
+        sums = data_batch.sum(1).unsqueeze(1)
+        if args.bow_norm:
+            normalized_data_batch = data_batch / sums
+        else:
+            normalized_data_batch = data_batch
+        recon_loss, kld_theta = model(data_batch, normalized_data_batch)
+        total_loss = recon_loss + kld_theta
+        total_loss.backward()
+
+        if args.clip > 0:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
+        optimizer.step()
+
+        acc_loss += torch.sum(recon_loss).item()
+        acc_kl_theta_loss += torch.sum(kld_theta).item()
+        cnt += 1
+
+        if idx % args.log_interval == 0 and idx > 0:
+            cur_loss = round(acc_loss / cnt, 2) 
+            cur_kl_theta = round(acc_kl_theta_loss / cnt, 2) 
+            cur_real_loss = round(cur_loss + cur_kl_theta, 2)
+
+            #print('Epoch: {} .. batch: {}/{} .. LR: {} .. KL_theta: {} .. Rec_loss: {} .. NELBO: {}'.format(
+                #epoch, idx, len(indices), optimizer.param_groups[0]['lr'], cur_kl_theta, cur_loss, cur_real_loss))
+    
+    cur_loss = round(acc_loss / cnt, 2)
+    cur_kl_theta = round(acc_kl_theta_loss / cnt, 2) 
+    cur_real_loss = round(cur_loss + cur_kl_theta, 2)
+
+    train_losses.append(cur_loss) 
+
+    print('*'*100)
+    print('Epoch----->{} .. LR: {} .. KL_theta: {} .. Rec_loss: {} .. NELBO: {}'.format(
+            epoch, optimizer.param_groups[0]['lr'], cur_kl_theta, cur_loss, cur_real_loss))
+    print('*'*100)
+
+def visualize(m, show_emb=True):
+    if not os.path.exists('./results'):
+        os.makedirs('./results')
+
+    m.eval()
+
+    queries = ['россия', 'человек', 'компьютер', 'спорт', 'религия', 'любовь', 
+                            'доллар', 'правительство', 'здоровье', 'семья']
+
+    ## visualize topics using monte carlo
+    with torch.no_grad():
+        print('#'*100)
+        print('Visualize topics...')
+        topics_words = []
+        gammas = m.get_beta()
+        for k in range(args.num_topics):
+            gamma = gammas[k]
+            top_words = list(gamma.cpu().numpy().argsort()[-args.num_words+1:][::-1])
+            topic_words = [vocab[a] for a in top_words]
+            topics_words.append(' '.join(topic_words))
+            print('Topic {}: {}'.format(k, topic_words))
+
+        if show_emb:
+            ## visualize word embeddings by using V to get nearest neighbors
+            print('#'*100)
+            print('Visualize word embeddings by using output embedding matrix')
+            try:
+                embeddings = m.rho.weight  # Vocab_size x E
+            except:
+                embeddings = m.rho         # Vocab_size x E
+            neighbors = []
+            for word in queries:
+                print('word: {} .. neighbors: {}'.format(
+                    word, nearest_neighbors(word, embeddings, vocab)))
+            print('#'*100)
+
+def evaluate(m, source, tc=False, td=False):
+    """Compute perplexity on document completion.
+    """
+    m.eval()
+    with torch.no_grad():
+        if source == 'val':
+            indices = torch.split(torch.tensor(range(args.num_docs_valid)), args.eval_batch_size)
+            tokens = valid_tokens
+            counts = valid_counts
+        else: 
+            indices = torch.split(torch.tensor(range(args.num_docs_test)), args.eval_batch_size)
+            tokens = test_tokens
+            counts = test_counts
+
+        ## get \beta here
+        beta = m.get_beta()
+
+        ### do dc and tc here
+        acc_loss = 0
+        cnt = 0
+        indices_1 = torch.split(torch.tensor(range(args.num_docs_test_1)), args.eval_batch_size)
+        for idx, ind in enumerate(indices_1):
+            ## get theta from first half of docs
+            data_batch_1 = data.get_batch(test_1_tokens, test_1_counts, ind, args.vocab_size, device)
+            sums_1 = data_batch_1.sum(1).unsqueeze(1)
+            if args.bow_norm:
+                normalized_data_batch_1 = data_batch_1 / sums_1
+            else:
+                normalized_data_batch_1 = data_batch_1
+            theta, _ = m.get_theta(normalized_data_batch_1)
+
+            ## get prediction loss using second half
+            data_batch_2 = data.get_batch(test_2_tokens, test_2_counts, ind, args.vocab_size, device)
+            sums_2 = data_batch_2.sum(1).unsqueeze(1)
+            res = torch.mm(theta, beta)
+            preds = torch.log(res)
+            recon_loss = -(preds * data_batch_2).sum(1)
+            
+            loss = recon_loss / sums_2.squeeze()
+            loss = loss.mean().item()
+            acc_loss += loss
+            cnt += 1
+        cur_loss = acc_loss / cnt
+        print('{} loss: {}'.format(source.upper(), cur_loss))
+        val_losses.append(cur_loss) 
+    
+        ppl_dc = round(math.exp(cur_loss), 1)
+        print('*'*100)
+        print('{} Doc Completion PPL: {}'.format(source.upper(), ppl_dc))
+        print('*'*100)
+        if tc or td:
+            beta = beta.data.cpu().numpy()
+            if tc:
+                print('Computing topic coherence...')
+                get_topic_coherence(beta, train_tokens, vocab)
+            if td:
+                print('Computing topic diversity...')
+                get_topic_diversity(beta, 25)
+        return ppl_dc
+
+def prepare_viz_data(phi, theta, n_wd, vocab):
+    theta = theta / theta.sum(axis=1, keepdims=1)
+    data = {'topic_term_dists': phi, 
+            'doc_topic_dists': theta,
+            'doc_lengths': n_wd.sum(axis=1).tolist(), 
+            'vocab': vocab,
+            'term_frequency': n_wd.sum(axis=0).tolist()} 
+    return data
+
+def show_viz(model, data_batch, vocab):
+  phi = model.get_beta().cpu().numpy()
+  theta, _  = model.get_theta(data_batch)
+  theta = theta.cpu().numpy()
+  model_data = prepare_viz_data(phi, theta, data_batch, vocab)
+  model_viz = pyLDAvis.prepare(**model_data)
+  pyLDAvis.save_html(model_viz, 'etm_vis.html')
+  print('\n\nVisualization has been saved')
+
+def plot_curves():
+  fig, axs = plt.subplots(1, 2)
+  axs[0].plot(train_losses)
+  axs[0].set_title('train')
+  axs[1].plot(val_losses)
+  axs[1].set_title('val')
+  plt.ylabel('loss values')
+  plt.xlabel('epoch')
+  plt.show()
+
 if args.mode == 'train':
     ## train model on data 
     best_epoch = 0
     best_val_ppl = 1e9
     all_val_ppls = []
     print('\n')
-    print('Visualizing model quality before training...', args.epochs)
-    #model.visualize(args, vocabulary = vocab)
+    print('Visualizing model quality before training...')
+    visualize(model)
     print('\n')
-    for epoch in range(0, args.epochs):
-        print("I am training for epoch", epoch)
-        model.train_for_epoch(epoch, args, training_set)
-        val_ppl = model.evaluate(args, 'val', training_set, vocab,  test_1, test_2)
-        print("The validation scores", val_ppl)
+    for epoch in range(1, args.epochs):
+        train(epoch)
+        val_ppl = evaluate(model, 'val')
         if val_ppl < best_val_ppl:
             with open(ckpt, 'wb') as f:
                 torch.save(model, f)
@@ -158,13 +357,16 @@ if args.mode == 'train':
             if args.anneal_lr and (len(all_val_ppls) > args.nonmono and val_ppl > min(all_val_ppls[:-args.nonmono]) and lr > 1e-5):
                 optimizer.param_groups[0]['lr'] /= args.lr_factor
         if epoch % args.visualize_every == 0:
-            model.visualize(args, vocabulary = vocab)
+            visualize(model)
         all_val_ppls.append(val_ppl)
+
+    plot_curves()
+
     with open(ckpt, 'rb') as f:
         model = torch.load(f)
     model = model.to(device)
-    val_ppl = model.evaluate(args, 'val', training_set, vocab,  test_1, test_2)
-else:   
+    val_ppl = evaluate(model, 'val')
+elif args.mode == 'eval':   
     with open(ckpt, 'rb') as f:
         model = torch.load(f)
     model = model.to(device)
@@ -172,15 +374,16 @@ else:
 
     with torch.no_grad():
         ## get document completion perplexities
-        test_ppl = model.evaluate(args, 'val', training_set, vocab,  test_1, test_2)
+        test_ppl = evaluate(model, 'test', tc=args.tc, td=args.td)
+
         ## get most used topics
         indices = torch.tensor(range(args.num_docs_train))
         indices = torch.split(indices, args.batch_size)
         thetaAvg = torch.zeros(1, args.num_topics).to(device)
-        theta_weighted_average = torch.zeros(1, args.num_topics).to(device)
+        thetaWeightedAvg = torch.zeros(1, args.num_topics).to(device)
         cnt = 0
-        for idx, indice in enumerate(indices):
-            data_batch = data.get_batch(training_set, indice, device)
+        for idx, ind in enumerate(indices):
+            data_batch = data.get_batch(train_tokens, train_counts, ind, args.vocab_size, device)
             sums = data_batch.sum(1).unsqueeze(1)
             cnt += sums.sum(0).squeeze().cpu().numpy()
             if args.bow_norm:
@@ -190,11 +393,11 @@ else:
             theta, _ = model.get_theta(normalized_data_batch)
             thetaAvg += theta.sum(0).unsqueeze(0) / args.num_docs_train
             weighed_theta = sums * theta
-            theta_weighted_average += weighed_theta.sum(0).unsqueeze(0)
+            thetaWeightedAvg += weighed_theta.sum(0).unsqueeze(0)
             if idx % 100 == 0 and idx > 0:
                 print('batch: {}/{}'.format(idx, len(indices)))
-        theta_weighted_average = theta_weighted_average.squeeze().cpu().numpy() / cnt
-        print('\nThe 10 most used topics are {}'.format(theta_weighted_average.argsort()[::-1][:10]))
+        thetaWeightedAvg = thetaWeightedAvg.squeeze().cpu().numpy() / cnt
+        print('\nThe 10 most used topics are {}'.format(thetaWeightedAvg.argsort()[::-1][:10]))
 
         ## show topics
         beta = model.get_beta()
@@ -212,13 +415,59 @@ else:
                 rho_etm = model.rho.weight.cpu()
             except:
                 rho_etm = model.rho.cpu()
-            queries = ['felix', 'covid', 'pprd', '100jours', 'beni', 'adf', 'muyembe', 'fally']
+            queries = ['россия', 'человек', 'компьютер', 'спорт', 'религия', 'любовь', 
+                            'доллар', 'правительство', 'здоровье', 'семья']
             print('\n')
             print('ETM embeddings...')
             for word in queries:
                 print('word: {} .. etm neighbors: {}'.format(word, nearest_neighbors(word, rho_etm, vocab)))
             print('\n')
+else:
+  with open(ckpt, 'rb') as f:
+        model = torch.load(f)
+  model = model.to(device)
+  model.eval()
 
-current, peak = tracemalloc.get_traced_memory()
-print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
-tracemalloc.stop()
+  with torch.no_grad():
+        ## get document completion perplexities
+
+        indices = torch.split(torch.tensor(range(args.num_docs_test)), args.eval_batch_size)
+        tokens = test_tokens
+        counts = test_counts
+
+        beta = model.get_beta()
+        beta[beta < 9e-7] = 0
+
+        ppx = 0
+        nd = 0
+        indices_1 = torch.split(torch.tensor(range(args.num_docs_test_1)), args.eval_batch_size)
+        for idx, ind in enumerate(indices_1):
+
+            data_batch_1 = data.get_batch(test_1_tokens, test_1_counts, ind, args.vocab_size, device)
+            theta, _ = model.get_theta(data_batch_1)
+            theta[theta < 5e-5] = 0
+
+            np.seterr(divide = 'ignore') 
+            p_wd = np.matmul(theta.cpu().numpy(), beta.cpu().numpy())
+            p_wd = np.log(p_wd)
+            p_wd[np.isneginf(p_wd)] = 0
+            recon_loss = -1 * np.multiply(p_wd,data_batch_1.cpu().numpy()).sum()
+
+            sums = data_batch_1.sum()
+            ppx +=  recon_loss.item()
+            nd += sums
+            
+        avg_ppx = ppx / nd
+        res_ppx = math.exp(avg_ppx)
+
+        indices = torch.split(torch.tensor(range(args.num_docs_test_1)), 1000)
+        data_batch = data.get_batch(test_1_tokens, test_1_counts, indices[0], args.vocab_size, device)
+        show_viz(model, data_batch, vocab)
+        print(f'result perplexity: {res_ppx}')
+        print(f'beta sparsity: {1.0 - torch.count_nonzero(beta)  / torch.numel(beta)}')
+        print(f'theta sparsity: {1.0 - torch.count_nonzero(theta)  / torch.numel(theta)}')
+        get_topic_diversity(beta, 10)
+        print(f'Coherence c_v: {get_coherence_gensim(vocab, data_batch, beta, "c_v")}')
+        print(f'Coherence c_nmpi: {get_coherence_gensim(vocab, data_batch, beta, "c_npmi")}')
+
+       
