bf7b2234330483773b3011a47fa7b6deefd1bf30
pacifikus <masterkristall@gmail.com>
2021-08-28 16:39:58 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
-import torch 
 import numpy as np
+import torch
 from gensim.corpora import Dictionary
 from gensim.models.coherencemodel import CoherenceModel
 
@@ -8,8 +8,8 @@ def get_topic_diversity(beta, topk):
     num_topics = beta.shape[0]
     list_w = np.zeros((num_topics, topk))
     for k in range(num_topics):
-      idx = beta[k,:].argsort()[-topk:][::-1]
-      list_w[k,:] = idx
+        idx = beta[k, :].argsort()[-topk:][::-1]
+        list_w[k, :] = idx
     n_unique = len(np.unique(list_w))
     TD = n_unique / (topk * num_topics)
     print('Topic diversity is: {}'.format(TD))
@@ -20,19 +20,19 @@ def get_topics(vocabulary, beta, dictionary, topk=10):
     num_topics = beta.shape[0]
     list_w = np.zeros((num_topics, topk))
     for k in range(num_topics):
-      idx = beta[k,:].argsort()[-topk:][::-1]
-      list_w[k,:] = idx
+        idx = beta[k, :].argsort()[-topk:][::-1]
+        list_w[k, :] = idx
 
     word_topics = []
     for topic in list_w:
-      word_topics.append([vocabulary[int(i)] for i in topic])
+        word_topics.append([vocabulary[int(i)] for i in topic])
     return word_topics
 
 
 def get_dictionary(vocabulary, n_wd):
     docs = []
     for row in n_wd:
-      docs.append([vocabulary[i] for i, value in enumerate(row) if value > 0])
+        docs.append([vocabulary[i] for i, value in enumerate(row) if value > 0])
     return Dictionary(docs), docs
 
 
@@ -42,12 +42,13 @@ def get_coherence_gensim(word_topics, dictionary, c_type, docs):
     coherence = coherence_model.get_coherence()
     return coherence
 
+
 def get_document_frequency(data, wi, wj=None):
     if wj is None:
         D_wi = 0
         for l in range(len(data)):
             doc = data[l].squeeze(0)
-            if len(doc) == 1: 
+            if len(doc) == 1:
                 continue
             else:
                 doc = doc.squeeze()
@@ -58,7 +59,7 @@ def get_document_frequency(data, wi, wj=None):
     D_wi_wj = 0
     for l in range(len(data)):
         doc = data[l].squeeze(0)
-        if len(doc) == 1: 
+        if len(doc) == 1:
             doc = [doc.squeeze()]
         else:
             doc = doc.squeeze()
@@ -66,10 +67,11 @@ def get_document_frequency(data, wi, wj=None):
             D_wj += 1
             if wi in doc:
                 D_wi_wj += 1
-    return D_wj, D_wi_wj 
+    return D_wj, D_wi_wj
+
 
 def get_topic_coherence(beta, data, vocab):
-    D = len(data) ## number of docs...data is list of documents
+    D = len(data)  ## number of docs...data is list of documents
     print('D: ', D)
     TC = []
     num_topics = len(beta)
@@ -91,28 +93,29 @@ def get_topic_coherence(beta, data, vocab):
                 if D_wi_wj == 0:
                     f_wi_wj = -1
                 else:
-                    f_wi_wj = -1 + ( np.log(D_wi) + np.log(D_wj)  - 2.0 * np.log(D) ) / ( np.log(D_wi_wj) - np.log(D) )
+                    f_wi_wj = -1 + (np.log(D_wi) + np.log(D_wj) - 2.0 * np.log(D)) / (np.log(D_wi_wj) - np.log(D))
                 # update tmp: 
                 tmp += f_wi_wj
                 j += 1
                 counter += 1
             # update TC_k
-            TC_k += tmp 
+            TC_k += tmp
         TC.append(TC_k)
     print('counter: ', counter)
     print('num topics: ', len(TC))
     TC = np.mean(TC) / counter
     print('Topic coherence is: {}'.format(TC))
 
+
 def nearest_neighbors(word, embeddings, vocab):
-    vectors = embeddings.data.cpu().numpy() 
+    vectors = embeddings.data.cpu().numpy()
     index = vocab.index(word)
     print('vectors: ', vectors.shape)
     query = vectors[index]
     print('query: ', query.shape)
     ranks = vectors.dot(query).squeeze()
     denom = query.T.dot(query).squeeze()
-    denom = denom * np.sum(vectors**2, 1)
+    denom = denom * np.sum(vectors ** 2, 1)
     denom = np.sqrt(denom)
     ranks = ranks / denom
     mostSimilar = []
